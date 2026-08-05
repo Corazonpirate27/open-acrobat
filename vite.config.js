187d2c4bbdf2f8eca94607c8acaed20bc5c83f7f
Corazonpirate27 <arogyabaral123@gmail.com>
2026-08-05 13:04:02 +0545

    Set relative base path for CSS & JS assets in Electron

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: './', // Ensures CSS, JS, and worker assets load properly in Electron and relative file environments
   plugins: [
     tailwindcss(),
   ],
